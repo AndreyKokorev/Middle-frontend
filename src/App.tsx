4d39b0fe098937d64da12686c916f3520d6f6b3e
andrey.kokorev <andrey.kokorev.w.dev@gmail.com>
2023-02-04 20:36:10 +0700

    feat: добавлено тёмная и светлая темы

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import Counter from "./components/Counter.jsx"
 import { Routes, Route, Link } from "react-router-dom"
-import './index.scss'
+import './styles/index.scss'
 import {HomePageAsync} from "./pages/HomePage/HomePageAsync";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {Suspense} from "react";
+import {useTheme} from "./theme/useTheme";
+
 
 const App = () => {
+    const { theme, toggleTheme } = useTheme()
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle theme</button>
             <Counter/>
             <Link to={'/'}> Главная</Link>
             <Link to={'/about'}> О сайте</Link>
