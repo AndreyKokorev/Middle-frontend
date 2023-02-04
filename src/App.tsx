52f687f7470df753956d0d558ca25feda5ca4c39
andrey.kokorev <andrey.kokorev.w.dev@gmail.com>
2023-02-04 17:57:51 +0700

    feat: добавлен роутинг и страницы вынесены в отдельные чанки

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
 import Counter from "./components/Counter.jsx"
+import { Routes, Route, Link } from "react-router-dom"
 import './index.scss'
+import {HomePageAsync} from "./pages/HomePage/HomePageAsync";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {Suspense} from "react";
 
 const App = () => {
     return (
         <div className="app">
             <Counter/>
+            <Link to={'/'}> Главная</Link>
+            <Link to={'/about'}> О сайте</Link>
+            <Suspense fallback={<div>...loading</div>} >
+                <Routes>
+                    <Route path={'/'} element={<HomePageAsync />}/>
+                    <Route path={'/about'} element={<AboutPageAsync />}/>
+                </Routes>
+            </Suspense>
+
         </div>
     )
 }
